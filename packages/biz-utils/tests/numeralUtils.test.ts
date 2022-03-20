0fbe1fc9e76af191b05f4002d92e0ecb2ff1b3de
hstarorg <hm910705@163.com>
2022-03-21 00:11:00 +0800

    test: add numeral utils's tests

--- packages/biz-utils/tests/numeralUtils.test.ts
+++ packages/biz-utils/tests/numeralUtils.test.ts
@@ -1,4 +1,4 @@
-import { numberAdd } from '../src';
+import { numberAdd, numberDiv, numberMul, numberSub } from '../src';
 
 describe('numeralUtils test', () => {
   describe('numberAdd test', () => {
@@ -9,6 +9,39 @@ describe('numeralUtils test', () => {
 
     it('add many times', () => {
       expect(numberAdd(0.1, 0.2, 0.3, 0.4)).toBe('1');
+    });
+  });
+
+  describe('numberSub test', () => {
+    it('sub once', () => {
+      expect(numberSub(0.1, 0.2)).toBe('-0.1');
+      expect(numberSub(0.2, 0.4)).toBe('-0.2');
+    });
+
+    it('sub many times', () => {
+      expect(numberSub(0.1, 0.2, 0.3, 0.4)).toBe('-0.8');
+    });
+  });
+
+  describe('numberMul test', () => {
+    it('mul once', () => {
+      expect(numberMul(0.1, 2)).toBe('0.2');
+      expect(numberMul(0.2, 2)).toBe('0.4');
+    });
+
+    it('mul many times', () => {
+      expect(numberMul(0.1, 2, 5, '33')).toBe('33');
+    });
+  });
+
+  describe('numberDiv test', () => {
+    it('div once', () => {
+      expect(numberDiv(1, 8)).toBe('0.125');
+      expect(numberDiv(0.2, 2)).toBe('0.1');
+    });
+
+    it('div many times', () => {
+      expect(numberDiv(10, 2, 2, 2)).toBe('1.25');
     });
   });
 });
